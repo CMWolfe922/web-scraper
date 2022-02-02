b633eb6490408a7d2273c1896015e23864609ac3
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-02-02 10:48:59 -0600

    Updated webbot and the LF was replaced by CRLF

---
+++ webbot/webbot/items.py
@@ -0,0 +1,21 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class WebbotItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class Articles(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    url = scrapy.Field()
+    author = scrapy.Field()
+    body = scrapy.Field()
+    publishDate = scrapy.Field()
+    lastUpdated = scrapy.Field()
---
+++ webbot/webbot/spiders/marketNews.py
@@ -0,0 +1,36 @@
+#!/.venv/bin/activate python3
+
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from webbot.items import Articles
+
+class MarketnewsSpider(CrawlSpider):
+    name = 'marketNews'
+    allowed_domains = ['cnbc.com']
+    start_urls = ['http://cnbc.com/']
+
+    # ------------ Create regex pattern to follow correct urls ------------ #
+    url_pattern = ''
+
+    # --------------- Create custom settings for this spider -------------- #
+    custom_settings = {
+        'FEED_URI': 'articles.csv',  # This can be json, or xml
+        'FEED_FORMAT': 'csv',  # json or xml
+        'CLOSESPIDER_PAGECOUNT': 100
+    }
+
+    # ---------- I need to create rules for the spider to follow ---------- #
+    rules = [Rule(LinkExtractor(), callback='parse_info', follow=True)]
+
+    def parse_info(self, response):
+        """Create the items to scrape from each url using xpath"""
+        article = Articles()
+
+        articles['title'] = response.xpath('//h1[@class="ArticleHeader-headline"]/text()')
+        articles['url'] = response.url
+        articles['author'] = response.xpath('//*[@class="Author-authorName"]/text()')
+        articles['body'] = response.xpath('//*[@id="RegularArticle-ArticleBody-5"]/div//p/text()')
+        # articles['publishDate'] = response.xpath()
+        # articles['lastUpdated'] = response.xpath()
+

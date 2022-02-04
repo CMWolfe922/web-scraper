8b41576de9e313fe0cd34141fbc3b08d78c1fc30
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-02-04 12:28:58 -0600

    mysql pipeline basic structure built

--- meetings/meetings/pipelines.py
+++ meetings/meetings/pipelines.py
@@ -6,9 +6,22 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import mysql.connector
 
 class MeetingsPipeline:
+
+    table_query = f"""CREATE TABLE IF NOT EXISTS {table}({schema})"""
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = mysql.connector.connect()
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute(self.table_query)
 
     def process_item(self, item, spider):
         return item

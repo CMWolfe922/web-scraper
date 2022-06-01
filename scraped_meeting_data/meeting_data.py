07ff3515db8be8d22d4fb5d11fa332447f685d6b
Charles Wolfe <cmwolfe.dev@gmail.com>
2022-05-31 22:22:07 -0500

    Added an if __name__ block to the file, and added a program timer. Also, the program is scraping the entire link_list now. then I will have to come up with a way to convert it to dataframe and create csv

--- scraped_meeting_data/meeting_data.py
+++ scraped_meeting_data/meeting_data.py
@@ -1,10 +1,9 @@
 import sqlite3 as sql
 import csv
-import json
-import re
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+import time
 import lxml
 
 
@@ -80,12 +79,7 @@ address_ending_strings = ['Ave', 'Blvd', 'Street', 'Dr',
                           'Ln', 'Rd', 'Road', 'Highway', 'Hwy', 'Pkwy', 'St']
 
 
-def meeting_data_scraper(link):
-    data = {'name': [], 'address': [], 'city': [], 'state': [], 'zip_code': [
-    ], 'details': {'schedule': {'day': [], 'time': [], 'info': []}}}
-    # get the requests object from the link
-    page = requests.get(link)
-    soup = bs(page.text, "lxml")
+def meeting_data_scraper(link_list):
 
     def get_address_data(soup, data):
         address_tag = soup.address
@@ -99,8 +93,6 @@ def meeting_data_scraper(link):
         data['state'].append(state_tag.contents[0])
         return data
 
-    get_address_data(soup, data)
-
     def get_table_data(soup, data):
         info_table = soup.find('table', class_='table fui-table')
         # obtain all the columns from <th>
@@ -110,80 +102,55 @@ def meeting_data_scraper(link):
             headers.append(title.lower())
 
             # now create a dataframe:
-        table_data = pd.DataFrame(columns=headers)
+        df = pd.DataFrame(columns=headers)
 
         # Now create the foor loop to fill dataframe
         # a row is under the <tr> tags and data under the <td> tags
         for j in info_table.find_all('tr')[1:]:
             row_data = j.find_all('td')
             row = [i.text for i in row_data]
-            length = len(table_data)
-            table_data.loc[length] = row
+            length = len(df)
+            df.loc[length] = row
 
-        print(table_data)
+        data['day'].append(df['day'].to_list())
+        data['time'].append(df['time'].to_list())
+        data['info'].append(df['info'].to_list())
 
-    get_table_data(soup, data)
+        # now return data
+        return data
+
+    _data = {'name': [], 'address': [], 'city': [], 'state': [], 'zip_code': [
+    ], 'day': [], 'time': [], 'info': []}
+
+    # start for loop:
+    for i in range(len(link_list)):
+        # get the requests object from the link
+        page = requests.get(link_list[i])
+        soup = bs(page.text, "lxml")
+
+        data = get_address_data(soup, _data)
+        data = get_table_data(soup, data)
+
+    return data
 
 
-csv_filename = 'scraped_meeting_data/meetings.csv'
-states = {
-    'AK': 'Alaska',
-    'AL': 'Alabama',
-    'AR': 'Arkansas',
-    'AZ': 'Arizona',
-    'CA': 'California',
-    'CO': 'Colorado',
-    'CT': 'Connecticut',
-    'DC': 'District of Columbia',
-    'DE': 'Delaware',
-    'FL': 'Florida',
-    'GA': 'Georgia',
-    'HI': 'Hawaii',
-    'IA': 'Iowa',
-    'ID': 'Idaho',
-    'IL': 'Illinois',
-    'IN': 'Indiana',
-    'KS': 'Kansas',
-    'KY': 'Kentucky',
-    'LA': 'Louisiana',
-    'MA': 'Massachusetts',
-    'MD': 'Maryland',
-    'ME': 'Maine',
-    'MI': 'Michigan',
-    'MN': 'Minnesota',
-    'MO': 'Missouri',
-    'MS': 'Mississippi',
-    'MT': 'Montana',
-    'NC': 'North Carolina',
-    'ND': 'North Dakota',
-    'NE': 'Nebraska',
-    'NH': 'New Hampshire',
-    'NJ': 'New Jersey',
-    'NM': 'New Mexico',
-    'NV': 'Nevada',
-    'NY': 'New York',
-    'OH': 'Ohio',
-    'OK': 'Oklahoma',
-    'OR': 'Oregon',
-    'PA': 'Pennsylvania',
-    'RI': 'Rhode Island',
-    'SC': 'South Carolina',
-    'SD': 'South Dakota',
-    'TN': 'Tennessee',
-    'TX': 'Texas',
-    'UT': 'Utah',
-    'VA': 'Virginia',
-    'VT': 'Vermont',
-    'WA': 'Washington',
-    'WI': 'Wisconsin',
-    'WV': 'West Virginia',
-    'WY': 'Wyoming'
-}
-address_ending_strings = ['Ave', 'Blvd', 'Street', 'Dr',
-                          'Ln', 'Rd', 'Road', 'Highway', 'Hwy', 'Pkwy', 'St']
+if __name__ == "__main__":
+    # program start:
+    start = time.time()
 
-with open(csv_filename, 'r') as f:
-    csv_reader = csv.reader(f)
-    link_list = csv_parser(csv_reader, 'link')
+    # Open csv file and get list of all the url links
+    with open(csv_filename, 'r') as f:
+        csv_reader = csv.reader(f)
+        link_list = csv_parser(csv_reader, 'link')
 
-meeting_data_scraper(link_list[4])
+    # Now use the scraper function to scrape each link in that list
+    # and store the data in the d variable for dict
+    d = meeting_data_scraper(link_list)
+
+    # print d to show it worked
+    print(d)
+
+    # end of program timer
+    end = time.time()
+    t = end - start
+    print(f"Program took: {t} seconds")

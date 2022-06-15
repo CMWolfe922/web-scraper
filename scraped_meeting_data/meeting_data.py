e41af4f6e6230262fe9beed2605854dbfc04d548
C M Wolfe <cmwolfe.dev@gmail.com>
2022-06-15 07:59:28 -0500

    Updated the module to include the new pandas scrape function

--- scraped_meeting_data/meeting_data.py
+++ scraped_meeting_data/meeting_data.py
@@ -6,7 +6,6 @@ import itertools as it
 from loguru import logger
 import time
 import humanize
-import os.path
 # Move the CSV file and the logger
 # this is where any files or directories/path variables go:
 csv_filename = './meetings.csv'
@@ -152,61 +151,46 @@ def get_table_data(soup):
         logger.error("info_table.find_all('tr') failed: {}", ae)
         return {'day': 'day', 'time': 'time', 'info': 'info'}
 
-
-def row_parser(item0, item1):
+def parse_dicts(item0, item1):
     """
     :param item0: This is the address data in a dictionary. Use the following keys to access
-    the data -> Keys: 'name' - 'address' - 'city' - 'state'
+    the data -> Keys: 'name' - 'address' - 'city' - 'state' 
     :param item1: This is the meeting details data in a dictionary. Use the following keys to
     access the data -> Keys: 'day' - 'time' - 'info'
-
-    create a final dictionary that will be used to store the information in the database as one row.
-    I will need to join the list items to create one string with a | seperating each item so I can
+    
+    create a final dictionary that will be used to store the information in the database as one row. 
+    I will need to join the list items to create one string with a | seperating each item so I can 
     split the string when retrieving the data.
     """
-    row = []
+    row = {'name':[], 'address':[], 'city':[], 'state':[], 'zip_code':[], 'day':[], 'time':[], 'info':[]}
     try:
-        row.append(item0['name'])
-        row.append(item0['address'])
-        row.append(item0['city'])
-        row.append(item0['state'])
-        row.append('00000')
+
+        try:
+            row['name'].append(item0['name'])
+            row['address'].append(item0['address'])
+            row['city'].append(item0['city'])
+            row['state'].append(item0['state'])
+            row['zip_code'].append('00000')
+        except Exception as e:
+            logger.error("[-] Row Data Raised Exception: {}", e)
+            print(e)
+            row['name'].append('name')
+            row['address'].append('address')
+            row['city'].append('city')
+            row['state'].append('state')
+            row['zip_code'].append('zip_code')
+
+        # now add item1 to the row data
+        row['day'].append(' | '.join(item1['day']))
+        row['time'].append(' | '.join(item1['time']))
+        row['info'].append(' | '.join(item1['info']))
+
         logger.info("[+] Row Data Parsed")
+        return row
     except Exception as e:
-        logger.error("[-] Row Data Raised Exception: {}", e)
-        print(e)
-        row['name'] = 'name'
-        row['address'] = 'address'
-        row['city'] = 'city'
-        row['state'] = 'state'
-
-    try:
-        row.append('|'.join(item1['day']))
-        row.append('|'.join(item1['time']))
-        row.append('|'.join(item1['info']))
-    except Exception as e:
-        logger.error("[-] Row Data Raised Exception: {}", e)
-
-    # now return the row data dictionary
-
-    return row
-
+        logger.error("Exception Raised: {}", e)
 
 new_csv_file = './meeting_details.csv'
-headers = ["name", 'address', 'city', 'state',
-           'zip_code', 'day', 'time', 'info']
-
-
-def csv_writer(row_data, csv_filename, headers=None):
-    with open(csv_filename, 'a') as f:
-        if headers is not None:
-            writer = csv.writer(f, delimiter='|')
-            for row in row_data:
-                writer.writerow(row)
-        else:
-            writer = csv.writer(f, delimiter='|')
-            for row in row_data:
-                writer.writerow(row)
 
 #####################################################################################
 
@@ -224,37 +208,79 @@ link_list4 = link_list[30000:]
 # Test link list
 link_list_test = link_list[:1000]
 ####################################################################################
-# Single Soup Scraper for
+# Soup List Scraper
 ####################################################################################
 
 
-def single_soup_scraper(soup):
+def soup_list_scraper(soup_data):
     """
-    :param soup_data: This is the list of soup data that was returned by the scrape function.
+    :param soup_data: This is the list of soup data that was returned by the scrape function. 
 
-    :returns: a list of dictionaries that will get stored to the variable row since each dict
-    creates a row of data for the csv file.
+    :returns: a list of dictionaries that will get stored to the variable row since each dict 
+    creates a row of data for the csv file. 
     """
     try:
-        # Create two dicts with the following keys
-        address_dict = get_address_data(soup)
-        details_dict = get_table_data(soup)
-        d = [address_dict, details_dict]
-        row = row_parser(d[0], d[1])
-        logger.info(
-            "[+] Parsed Address and Table Data | Row [{}] Created", row)
-        return row
+        rows = {'name':[], 'address':[], 'city':[], 'state':[], 'zip_code':[], 'day':[], 'time':[], 'info':[]}
+        for soup in soup_data:
+            # Create two dicts with the following keys
+            address_dict = get_address_data(soup)
+            details_dict = get_table_data(soup)
+            logger.info("[+] Address and table data parsed")
+            d = [address_dict, details_dict]
+            row_data = parse_dicts(d[0], d[1])
+            rows['name'].append(row_data['name'])
+            rows['address'].append(row_data['address'])
+            rows['city'].append(row_data['city'])
+            rows['state'].append(row_data['state'])
+            rows['zip_code'].append(row_data['zip_code'])
+            rows['day'].append(row_data['day'])
+            rows['time'].append(row_data['time'])
+            rows['info'].append(row_data['info'])
+
+        logger.info("++++++++ SCRAPED SCRAPED BATCH OF LINKS +++++++++ ")
+        return rows
 
     except IndexError as ie:
         logger.error("{}: List Exhausted. No more Soup Items", ie)
 
     except Exception as e:
         logger.error("{}: Exception raised", e)
-
+        
 ####################################################################################
 # Now create the same logic above in the if __name__ == '__main__':
 ####################################################################################
+def scrape(link_list):
+    """
+    :param link_list: A list of links to scrape and get the soup data from the site, 
+    parse the data and append the soup data to the soup_data list. 
+    """
+    # Two empty lists. One for soup_data and another to store row_data
+    soup_data = []
+    # The headers for the csv_file
+    headers = ["name", 'address', 'city', 'state',
+               'zip_code', 'day', 'time', 'info']
+    try:
+        count = 0
+        for link in link_list:
+            soup = fetch_soup_data(link)
+            soup_data.append(soup)
+            logger.info(
+                "[+] {} scraped successfully: Link number {}", link, count)
+            count += 1
+            if count % 1000 == 0:
+                time.sleep(5)
+                # Every 1000 links, write the row data to the new CSV file
+                rows = soup_list_scraper(soup_data)
+                row_df = pd.DataFrame.from_dict(rows)
+                row_df.to_csv(new_csv_file, mode='a', sep="\t", index=False)
+                soup_data.clear()
+                logger.info(
+                            "[+] Count is divisible by 1000:{} | Writing ROW_DATA to [csv_file] | soup_data and row_data cleared!", count)
 
+        print(dataframes)
+        logger.info("<<<<<<<<<<<<<<< SUCCESS >>>>>>>>>>>>>>>>")
+    except IndexError as e:
+        logger.error("{}: List Exhausted. No more links to scrape.", e)
 
 if __name__ == '__main__':
     start = time.time()
@@ -266,56 +292,10 @@ if __name__ == '__main__':
     logger.add(log_file_path, rotation=rotation, level="INFO",
                format="[<green>{time: MMM D YYYY HH:mm:ss:SSSS}</>] | <level>{message}</>", backtrace=True, diagnose=True)
 
-    ##############################################################################
-    # Two empty lists. One for soup_data and another to store row_data
-    row_data = []
-    soup_data = []
-    # The headers for the csv_file
-    headers = ["name", 'address', 'city', 'state',
-               'zip_code', 'day', 'time', 'info']
     try:
-        count = 0
-        # Scrape Link List # 1:
-        for link in edited_list:
-            soup = fetch_soup_data(link)
-            soup_data.append(soup)
-            logger.info(
-                "[+] {} scraped successfully: Link number {}", link, count)
-            count += 1
-            if count % 500 == 0:
-                time.sleep(5)
-                # Every 500 links, write the row data to the new CSV file
-                for soup in soup_data:
-                    row = single_soup_scraper(soup)
-                    row_data.append(row)
-                    # check that row_data has 500 items:
-                    if len(row_data) == 500:
-                        try:
-                            if not os.path.exists(new_csv_file):
-                                df = pd.DataFrame(row_data, columns=headers)
-                                # after each soup item is parsed and the results appended to row_data, append row data to csv
-                                # csv_writer(row_data, new_csv_filename, headers)
-                                df.to_csv(new_csv_filename, sep='|',
-                                          mode='a', index=False)
-                                # Then clear both soup and row data lists
-                                soup_data.clear()
-                                row_data.clear()
-                                # log the success of the script
-                                logger.info(
-                                    "[+] Count is divisible by 500:{} | Writing ROW_DATA to [{}] | soup_data and row_data cleared!", count, new_csv_filename)
-                            else:
-                                df = pd.DataFrame(row_data, columns=headers)
-                                df.to_csv(new_csv_filename, sep='|',
-                                          mode='a', index=False)
-                                soup_data.clear()
-                                row_data.clear()
-                                logger.info(
-                                    "[+] Count divisible by 500:{} | Row Data wrote to: {} | soup_data and row_data cleared!", count, new_csv_filename)
-                        except TypeError as te:
-                            logger.error("TypeError Raised: {}", te)
+        scrape(link_list)
         end = time.time()
-        finish = (end - start)
-        program_timer = humanize.naturaltime(finish)
+        program_timer = humanize.naturaltime((end - start))
         logger.info(
             "<<<<<<<<<<<<<<< test_link_list: SUCCESS: PROGRAM STARTED {} >>>>>>>>>>>>>>>>", program_timer)
     except IndexError as e:

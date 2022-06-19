5ce91eb52b67ea032b8d697c90b2576d964f1762
C M Wolfe <cmwolfe.dev@gmail.com>
2022-06-18 23:08:15 -0500

    Updated the geopy address cleaner script

--- scraped_meeting_data/geopy_address_cleaner.py
+++ scraped_meeting_data/geopy_address_cleaner.py
@@ -5,6 +5,7 @@ import csv
 # csv file
 csv_file = './csv_files/meetings.csv'
 
+
 # CLEAN RAW ADDRESS DATA FROM FIRST CSV FILE:
 def create_list_from_column_data(csv_filename, column):
     """
@@ -22,28 +23,29 @@ def create_list_from_column_data(csv_filename, column):
     with open(csv_filename, 'r') as f:
         csv_reader = csv.reader(f)
         _column = next(csv_reader)
-        columnIndex = _column.index(column)
+        column_index = _column.index(column)
         # loop through CSV file and append to address_list
         for line in csv_reader:
-            all_data = line[columnIndex]
+            all_data = line[column_index]
             data_list.append(all_data)
     return data_list
 
 
-
-
-# USE EMAIL FOR FREE MAPS API
-geolocator = Nominatim(user_agent="cmwolfe.dev@gmail.com")
-
 def extract_clean_address(addresses):
+    # USE EMAIL FOR FREE MAPS API
+    geolocator = Nominatim(user_agent="sobering")
     try:
-        location = geolocator.geocode.OpenStreetMap(addresses, api_key='GOh7tfINgq3AUEcEzj2OAL42EHrI7U4L0QuJtvvK')
+        location = geolocator.geocode(addresses)
+        print(location.address)
         return location.address
-    except:
-        return ''
+    except Exception as e:
+        print(e)
+        return f"Exception Raised: {e}"
+
 
 if __name__ == '__main__':
-	address_list = create_list_from_column_data(csv_file, 'address')
-	messy_addresses = pd.DataFrame(address_list)
-	messy_addresses['cleaned_addresses'] = messy_addresses.apply(lambda x: extract_clean_address(x[0]) , axis =1  )
-	print(messy_addresses['cleaned_addresses'])
+    address_list = create_list_from_column_data(csv_file, 'address')
+    print(address_list)
+    messy_addresses = pd.DataFrame(address_list)
+    messy_addresses['cleaned_addresses'] = messy_addresses.apply(lambda x: extract_clean_address(x[0]), axis=1)
+    print(messy_addresses['cleaned_addresses'])

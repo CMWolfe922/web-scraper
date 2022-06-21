9d54e87e830b51816fe639f812feede14eaf1e5b
C M Wolfe <cmwolfe.dev@gmail.com>
2022-06-20 22:15:50 -0500

    Updated the geopy_address_cleaner, ran script and have the log file as well as started a log_parser

--- scraped_meeting_data/geopy_address_cleaner.py
+++ scraped_meeting_data/geopy_address_cleaner.py
@@ -1,5 +1,4 @@
 from geopy.geocoders import Nominatim
-import pandas as pd
 import csv
 from collections import deque
 from loguru import logger
@@ -10,7 +9,7 @@ csv_file = './meetings.csv'
 
 
 # Function to access csv file data:
-def create_list_from_column_data(csv_file, column):
+def create_list_from_column_data(csv_file, *columns):
     """
     :description: Create a list from a CSV column's data. To do this I will use the with open()
     context manager and open the csv file that was passed to this function as an argument.
@@ -26,11 +25,12 @@ def create_list_from_column_data(csv_file, column):
     with open(csv_file, 'r') as f:
         csv_reader = csv.reader(f)
         _column = next(csv_reader)
-        column_index = _column.index(column)
-        # loop through CSV file and append to address_list
-        for line in csv_reader:
-            all_data = line[column_index]
-            data_list.append(all_data)
+        for column in columns:
+            column_index = _column.index(column)
+            # loop through CSV file and append to address_list
+            for line in csv_reader:
+                all_data = line[column_index]
+                data_list.append(all_data)
     return data_list
 
 
@@ -48,8 +48,8 @@ if __name__ == '__main__':
     log_file_path = './data/address_cleaner.log'
     rotation = "10 MB"
     logger.add(log_file_path, rotation=rotation, level="INFO",
-               format="[<green>{time: MMM D YYYY HH:mm:ss.SS}</>] | {file} took {elapsed} to execute | <level>{message}</>", backtrace=True,
-               diagnose=True)
+               format="[<green>{time: MMM D YYYY HH:mm:ss.SS}</>]|{file} took {elapsed} to execute|<level>{message}</>",
+               backtrace=True, diagnose=True)
     name_list = create_list_from_column_data(csv_file, 'name')
     address_list = create_list_from_column_data(csv_file, 'address')
     nq = deque(name_list)
@@ -61,22 +61,24 @@ if __name__ == '__main__':
                 address = aq.popleft()
                 name = nq.popleft()
                 cleaned_address = extract_clean_address(address)
-                logger.info("<blue>[+] {} address cleaned: [{}]</> | <yellow>Index: [{}]</>", name, cleaned_address, count)
+                logger.success("[+] [meeting]: {} | [address]: {} | [Index]: [{}]", name, cleaned_address,
+                            count)
                 count += 1
 
             except Exception as e:
                 address = aq.popleft()
                 name = nq.popleft()
-                logger.error("<red>[-] {}'s address: [{}] NOT CLEANED</> | <yellow>Index: [{}]</>", name, address, count)
+                logger.error("[-] [meeting]: {} | [address]: {} <NOT CLEANED> | [Index]: [{}]", name, address,
+                             count)
                 count += 1
 
         end = time.time()
         timer = (end - start) / 60
         logger.success("<<<<<<<<<< Program Completed in {} minutes >>>>>>>>>>", timer)
     except StopIteration as si:
-        logger.error("<red>[-] Queue has been exhausted</>")
+        logger.error("[-] Queue has been exhausted")
     except Exception as e:
-        logger.error("<red>Exception Raised: {}</> @ <yellow>Index: [{}]</>", e, count)
+        logger.error("Exception Raised: {} @ Index: [{}]", e, count)
     # messy_addresses = pd.DataFrame(address_list)
     # messy_addresses['cleaned_addresses'] = messy_addresses.apply(lambda x: extract_clean_address(x[0]), axis=1)
     # print(messy_addresses['cleaned_addresses'])
--- scraped_meeting_data/log_parser.py
+++ scraped_meeting_data/log_parser.py
@@ -1,2 +1,12 @@
 #!/usr/bin/.venv python3.10
 from loguru import logger
+import re
+
+
+if __name__ == "__main__":
+    file = "./data/address_cleaner.log"
+    failed = re.findall(r"\[-]")
+    regex = r"([-])([^A-Z][a-z][$A-Za-z])"
+
+    for log in logger.parse(file, pattern=failed):
+        print(log)

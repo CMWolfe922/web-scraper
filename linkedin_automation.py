6e854ba0347daba414215d3bcf83117a62ccaed1
Charles Wolfe <cmwolfe.dev@gmail.com>
2024-01-01 08:17:59 -0600

    auto login is working. mpw I need the element selector objs

--- linkedin_automation.py
+++ linkedin_automation.py
@@ -7,94 +7,96 @@ import time
 from bs4 import BeautifulSoup as bs
 import os
 from loguru import logger
+# from dotenv import load_dotenv
 
+# load_dotenv()
 
-
-
-LINKEDIN_USERNAME = os.getenv("LINKEDIN_USERNAME")
-LINKEDIN_PASSWORD = os.getenv("LINKEDIN_PASSWORD")
 
 def open_firefox():
-	driver = webdriver.Firefox()
+    driver = webdriver.Firefox()
+
 
 def login_to_linkedin(driver):
-	# Webpage to automate
-	driver.get("https://www.linkedin.com/home")
-	# get the page title
-	title = driver.title
+    # Webpage to automate
+    driver.get("https://www.linkedin.com/home")
+    # get the page title
+    title = driver.title
 
-	# make the driver wait 5 seconds
-	driver.implicitly_wait(5)
+    # make the driver wait 5 seconds
+    driver.implicitly_wait(5)
 
-	# Create a text_box element
-	user_name = driver.find_element(by=By.ID, value="session_key")
-	password = driver.find_element(by=By.ID, value="session_password")
+    # Create a text_box element
+    user_name = driver.find_element(by=By.ID, value="session_key")
+    password = driver.find_element(by=By.ID, value="session_password")
 
-	# Create a submit_button element
-	submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button.btn-md:nth-child(3)")
+    # Create a submit_button element
+    submit_button = driver.find_element(
+        by=By.CSS_SELECTOR, value="button.btn-md:nth-child(3)")
 
-	# send username and password to the text boxes
-	user_name.send_keys(LINKEDIN_USERNAME)
-	time.sleep(1)
-	password.send_keys(LINKEDIN_PASSWORD)
-	time.sleep(2)
+    # send username and password to the text boxes
+    user_name.send_keys(os.getenv("LINKEDIN_USERNAME"))
+    time.sleep(1)
+    password.send_keys(os.getenv("LINKEDIN_PASSWORD"))
+    time.sleep(2)
 
-	# Submit the form submit button
-	submit_button.click()
+    # Submit the form submit button
+    submit_button.click()
+
 
 if __name__ == "__main__":
     # setting up logging
-	PATH = "./tmp/"
-	FILE = __file__.split('/')[-1]
-	LOG_FILE = os.path.join(PATH, FILE)
+    PATH = "./tmp/"
+    FILE = __file__.split('/')[-1]
+    LOG_FILE = os.path.join(PATH, FILE)
 
-	logger.add(LOG_FILE, format="{time:MM/DD/YYYY at HH:mm:ss} | {level} | {name} | {message}", diagnose=True, backtrace=True)
+    logger.add(
+        LOG_FILE, format="{time:MM/DD/YYYY at HH:mm:ss} | {level} | {name} | {message}", diagnose=True, backtrace=True)
 
-	# Check if the system is running on Windows or Linux
-	try:
+    # Check if the system is running on Windows or Linux
+    try:
 
-		if os.name == 'nt':
-			logger.info("Running on Windows")
+        if os.name == 'nt':
+            logger.info("Running on Windows")
 
-			# Set up headless Firefox
-			driver = webdriver.Firefox()
+            # Set up headless Firefox
+            driver = webdriver.Firefox()
 
-			# login function
-			login_to_linkedin(driver)
+            # login function
+            login_to_linkedin(driver)
 
-			# NOW I NEED TO GET DATA FROM THE LINKED IN PAGE:
-			# (I can use BeautifulSoup for this)
-			soup = driver.to_soup()
-			print(soup.parse_html())
+            # NOW I NEED TO GET DATA FROM THE LINKED IN PAGE:
+            # (I can use BeautifulSoup for this)
+            soup = driver.to_soup()
+            print(soup.parse_html())
 
-			# Get the message after the click
-			# text = text_box.text
-			time.sleep(3)
-			# Close the driver out
-			driver.quit()
+            # Get the message after the click
+            # text = text_box.text
+            time.sleep(3)
+            # Close the driver out
+            driver.quit()
 
-		elif os.name == 'posix':
+        elif os.name == 'posix':
 
-			logger.info("Running ona POSIX-compatiblesystem (Like Ubuntu)")
-			# Set up headless Firefox
-			executable_firefox = "/usr/bin/firefox"
-			service = Service(executable_path=executable_firefox)
-			options = FirefoxOptions()
-			options.add_argument("--headless")
-			driver = webdriver.Firefox(service=service, options=options)
-			# login function
-			login_to_linkedin(driver)
+            logger.info("Running ona POSIX-compatiblesystem (Like Ubuntu)")
+            # Set up headless Firefox
+            executable_firefox = "/usr/bin/firefox"
+            service = Service(executable_path=executable_firefox)
+            options = FirefoxOptions()
+            options.add_argument("--headless")
+            driver = webdriver.Firefox(service=service, options=options)
+            # login function
+            login_to_linkedin(driver)
 
-			# NOW I NEED TO GET DATA FROM THE LINKED IN PAGE:
-			# (I can use BeautifulSoup for this)
-			soup = driver.to_soup()
-			print(soup.parse_html())
+            # NOW I NEED TO GET DATA FROM THE LINKED IN PAGE:
+            # (I can use BeautifulSoup for this)
+            soup = driver.to_soup()
+            print(soup.parse_html())
 
-			# Get the message after the click
-			# text = text_box.text
-			time.sleep(3)
-			# Close the driver out
-			driver.quit()
+            # Get the message after the click
+            # text = text_box.text
+            time.sleep(3)
+            # Close the driver out
+            driver.quit()
 
-	except Exception as e:
-		logger.error(e)
+    except Exception as e:
+        logger.error(e)

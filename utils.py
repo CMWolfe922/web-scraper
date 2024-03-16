a194dea463ab38beafd9826a1775af7be8eaf1a2
Charles Wolfe <cmwolfe.dev@gmail.com>
2024-03-16 00:28:14 -0500

    Added a notebook and started building an Xpath builder, I have ten examples in the utils file and all the possible elements and attributes

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import json
 from http.server import BaseHTTPRequestHandler
 from io import StringIO
 import os
-import ColorizePython
+# import ColorizePython
 import mimetypes
 
 
@@ -39,11 +39,11 @@ def loadConfig(fileName):
         with open(fileName, 'r') as d:
             jsn = json.load(d)
             jsn['PUBLIC_HTML'] = os.path.normpath(
-                jsn['PUBLIC_HTML'])   # Normalize path
+                jsn['PUBLIC_HTML'])  # Normalize path
             jsn['ERROR_DIR'] = os.path.normpath(
-                jsn['ERROR_DIR'])   # Normalize path
+                jsn['ERROR_DIR'])  # Normalize path
             jsn['OTHER_TEMPLATES'] = os.path.normpath(
-                jsn['OTHER_TEMPLATES'])   # Normalize path
+                jsn['OTHER_TEMPLATES'])  # Normalize path
             return jsn
     except IOError:
         print("Error: File does not appear to exist.")
@@ -56,26 +56,26 @@ def loadConfig(fileName):
         sys.exit(1)
 
 
-def colorizeLog(shouldColorize, log_level, msg):
-    # Higher is the log_level in the log() argument, the lower is its priority.
-    colorize_log = {
-        "NORMAL": ColorizePython.pycolors.ENDC,
-        "WARNING": ColorizePython.pycolors.WARNING,
-        "SUCCESS": ColorizePython.pycolors.OKGREEN,
-        "FAIL": ColorizePython.pycolors.FAIL,
-        "RESET": ColorizePython.pycolors.ENDC
-    }
-
-    if shouldColorize.lower() == "true":
-        if log_level in colorize_log:
-            return colorize_log[str(log_level)] + msg + colorize_log['RESET']
-        return colorize_log["NORMAL"] + msg + colorize_log["RESET"]
-    return msg
-
-
-def guessMIME(filename):
-    return mimetypes.guess_type(filename)[0]
-
+# def colorizeLog(shouldColorize, log_level, msg):
+#     # Higher is the log_level in the log() argument, the lower is its priority.
+#     colorize_log = {
+#         "NORMAL": ColorizePython.pycolors.ENDC,
+#         "WARNING": ColorizePython.pycolors.WARNING,
+#         "SUCCESS": ColorizePython.pycolors.OKGREEN,
+#         "FAIL": ColorizePython.pycolors.FAIL,
+#         "RESET": ColorizePython.pycolors.ENDC
+#     }
+#
+#     if shouldColorize.lower() == "true":
+#         if log_level in colorize_log:
+#             return colorize_log[str(log_level)] + msg + colorize_log['RESET']
+#         return colorize_log["NORMAL"] + msg + colorize_log["RESET"]
+#     return msg
+#
+#
+# def guessMIME(filename):
+#     return mimetypes.guess_type(filename)[0]
+#
 
 def isvalidPath(location):
     """ Check if file/directory exists """
@@ -102,4 +102,65 @@ def isReadable(location):
     """ Check if file/directory is readable """
     if os.access(location, os.R_OK):
         return True
+    return False
+
+
+# Add the HTML Elements and Attribute Dictionary in here:
+# Create a method to check the elements and attributes lists to see if the user
+# put a valid element or attribute in the input prompt:
+# If the element is valid, continue to the next step
+
+element_attribute_examples = {
+    'Elements': [
+        "!DOCTYPE", "html", "head", "title", "base", "link", "meta", "style", "script", "noscript", "body", "section",
+        "nav",
+        "article", "aside", "h1", "h2", "h3", "h4", "h5", "h6", "header", "footer", "address", "main", "p", "hr", "pre",
+        "blockquote", "ol", "ul", "li", "dl", "dt", "dd", "figure", "figcaption", "div", "a", "em", "strong", "small",
+        "s", "cite",
+        "q", "dfn", "abbr", "ruby", "rt", "rp", "data", "time", "code", "var", "samp", "kbd", "sub", "sup", "i", "b",
+        "u",
+        "mark", "bdi", "bdo", "span", "br", "wbr", "ins", "del", "image", "img", "iframe", "embed", "object", "param",
+        "video",
+        "audio", "source", "track", "canvas", "map", "area", "svg", "math", "table", "caption", "colgroup", "col",
+        "tbody", "thead",
+        "tfoot", "tr", "td", "th", "form", "fieldset", "legend", "label", "input", "button", "select", "datalist",
+        "optgroup", "option",
+        "textarea", "keygen", "output", "progress", "meter", "details", "summary", "menu", "menuitem", "dialog"
+    ],
+    'Attributes': [
+        "id", "class", "style", "title", "alt", "src", "href", "target", "rel", "type", "value", "name", "placeholder",
+        "disabled", "checked", "readonly", "selected", "multiple", "required", "pattern", "min", "max", "step",
+        "data-*",
+        "aria-*", "role", "async", "defer", "srcset", "sizes", "hreflang", "charset", "autofocus", "autocomplete",
+        "novalidate",
+        "method", "action", "enctype", "formmethod", "formaction", "headers", "for", "form", "width", "height",
+        "frameborder",
+        "allow", "allowfullscreen", "autoplay", "loop", "muted", "controls", "download", "accesskey", "contenteditable",
+        "dir",
+        "draggable", "hidden", "lang", "spellcheck", "tabindex", "translate", "reversed", "start", "colspan", "rowspan",
+        "headers",
+        "scope", "align", "nowrap", "border", "cellpadding", "cellspacing", "summary", "usemap", "shape", "coords",
+        "poster",
+        "preload", "kind", "srclang", "sandbox", "integrity", "crossorigin", "referrerpolicy", "loading"
+    ],
+}
+
+xpath_examples={
+    "Select all elements with `//tagName`": '`//tagName`',
+    "Select all elements with `//tagName` with `attribute` with specific `value`: ": "`//tagName[@attribute='value']`",
+    "Select all elements with `//tagName` containing specific 'text content': ": "`//tagName[contains(text(),'text content')]`",
+    "Select nth `tagName` element in doc: ": '`//tagName[position()=n]`',
+    "Select first|last `childTagName` within `parentTagName`: ": '`//parentTagName/childTagName[1]` or `//parentTagName/childTagName[last()]`',
+    "Select elements that `attribute` = 'substring': ": "`//tagName[contains(@attribute, 'substring')]`",
+    "Select `tagName` ancestor|sibling element, showcasing XPath axes to navigate element relationships: ": '`//tagName/ancestor::ancestorTagName` or `//tagName/following-sibling::siblingTagName`',
+    "Use logical operators (and, or) to select elements that meet multiple conditions: ": "`//tagName[@attribute1='value1' and @attribute2='value2']`",
+    "Selects all tags with `className`, accounting for potential multiple class names.": "`//*[contains(concat(' ', normalize-space(@class), ' '), ' className ')]`",
+    "Select elements without certain `attribute` or select 'attribute' that exceeds the len of `n`( demonstrating the use of XPath functions for more complex filtering) :": '`//tagName[not(@attribute)]` or `//tagName[string-length(@attribute) > n]`'
+}
+
+def check_html_elements_and_attributes(html_dict:dict, user_input):
+    """ Check if the user input is a valid HTML element or attribute """
+    for key in html_dict:
+        if user_input in html_dict[key]:
+            return True
     return False
